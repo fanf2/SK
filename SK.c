c700959a62ba6a404d13849bf5f7f2ee2a9475d8
Tony Finch <dot@dotat.at>
2011-07-03 16:05:09 +0100

    Define a few primitive functions.

--- SK.c
+++ SK.c
@@ -1,33 +1,29 @@
-typedef union word word, cell[2];
+#include <stdbool.h>
+#include <stdlib.h>
 
-typedef void prim(cell app);
+typedef union word word, *cell;
+
+typedef cell prim(cell app);
 
 union word {
   word *ptr;
   prim *fun;
-  double num; /* only for the second word in a cell */
+  double num; /* only used in the second word of a cell */
 };
 
 word *heap_lo, *heap_ptr, *heap_hi;
 
 size_t heap_size = 1<<10;
 
-static inline cell cons(word w0, word w1) {
-  assert(heap_ptr < heap_hi);
-  cell cell = heap_ptr;
-  heap_ptr += 2;
-  cell[0] = w0;
-  cell[1] = w1;
-  return(cell);
-}
-
 /*
- * If a word doesn't point to the heap it must be a primitive function
+ * If a word doesn't point into the heap it must be a primitive function
  * (unless it is the second word of a cell containing a boxed number).
  */
 static inline bool primitive(word w) {
   return(w.ptr < heap_lo || heap_hi <= w.ptr);
 }
+
+static prim numberfun;
 
 static cell cheney(cell root) {
   word *new = malloc(heap_size * sizeof(*new));
@@ -41,7 +37,7 @@ static cell cheney(cell root) {
   for(word *here = new; here < ptr; here++) {
     if(primitive(*here)) {
       /* skip both words of a boxed number */
-      if(here->prim = number)
+      if(here->fun == numberfun)
 	here++;
       continue;
     }
@@ -53,30 +49,105 @@ static cell cheney(cell root) {
       here->ptr = ptr;
       ptr[0] = there[0];
       ptr[1] = there[1];
-      there[0] = NULL;
-      there[1] = ptr;
+      there[0].ptr = NULL;
+      there[1].ptr = ptr;
       ptr += 2;
     }
   }
 
+  free(heap_lo);
+
   heap_lo = new;
   heap_hi = new + heap_size;
   heap_ptr = ptr;
-  if(ptr - new > heap_size / 2)
+  if(ptr > new + heap_size / 2)
     heap_size *= 2;
 
   return(new);
 }
 
-static void eval(cell app) {
-  for(;;) {
-    if(primitive(app[0])) {
-      app = app[0].fun(app);
+static inline cell need(cell c, int n) {
+  if(heap_ptr + 2*n < heap_hi)
+    return(c);
+  else
+    return(cheney(c));
+}
+
+static inline word cons(word w0, word w1) {
+  cell c = heap_ptr;
+  c[0] = w0;
+  c[1] = w1;
+  heap_ptr += 2;
+  word w = { c };
+  return(w);
+}
+
+static void eval(cell c) {
+  while(c != NULL) {
+    if(primitive(c[0])) {
+      c = c[0].fun(c);
     } else {
-      cell next = app[0].ptr;
-      app[0] = next[1];
-      next[1].ptr = app;
-      app = next;
+      cell next = c[0].ptr;
+      c[0] = next[1];
+      next[1].ptr = c;
+      c = next;
     }
   }
+}
+
+static inline cell unwind(cell c, word *arg) {
+  cell prev = c[1].ptr;
+  *arg = c[1] = prev[0];
+  prev[0].ptr = c;
+  return(prev);
+}
+
+#define defprim(name) \
+  static prim name##fun; \
+  static word name = { .fun = name##fun }; \
+  static cell name##fun(cell c)
+
+defprim(I) {
+  word a1;
+  c = unwind(c, &a1);
+  c[0] = a1;
+  return(c);
+}
+
+defprim(J) {
+  word t, f;
+  c = unwind(c, &t);
+  c = unwind(c, &f);
+  c[0] = f;
+  return(c);
+}
+
+defprim(K) {
+  word t, f;
+  c = unwind(c, &t);
+  c = unwind(c, &f);
+  c[0] = t;
+  return(c);
+}
+
+defprim(S) {
+  word f, g, x;
+  c = need(c, 3);
+  c = unwind(c, &f);
+  c = unwind(c, &g);
+  c = unwind(c, &x);
+  c[0] = cons(cons(f,x),cons(g,x));
+  return(c);
+}
+
+defprim(number) {
+  word n, k;
+  c = unwind(c, &n);
+  /* actually we want the boxed number */
+  n = c[0];
+  c = unwind(c, &k);
+  /* pass evaluated number to continuation */
+  c[0].ptr[0] = k;
+  c[0].ptr[1] = n;
+  return(c);
 }
