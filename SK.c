75f2410bf0aa4551b92d89a90887f0184ce56406
Tony Finch <dot@dotat.at>
2011-07-03 21:59:00 +0100

    Better dump code; unwind/rewind jargon; prim_exit instead of NULL checks.

--- SK.c
+++ SK.c
@@ -4,19 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define rewind my_rewind
+
 typedef enum {
   primin,
-  prim_I, prim_J, prim_K, prim_S, prim_Y,
-  prim_number, prim_print, prim_putc, prim_getc,
+  special_forward = 0, special_number,
+  prim_Y, prim_I, prim_J, prim_K,
+  prim_S, prim_C, prim_B,
+  prim_SS, prim_CC, prim_BB,
+  prim_exit, prim_print, prim_putc, prim_getc,
   prim_floor, prim_ceil, prim_abs, prim_neg,
   prim_add, prim_sub, prim_mul, prim_div, prim_mod, prim_pow,
   primax
 } prim;
 
 static const char *primname[] = {
-  "nil",
-  "I", "J", "K", "S", "Y",
-  "number", "print", "putc", "getc",
+  "nil", "number",
+  "Y", "I", "J", "K",
+  "S", "C", "B", "SS", "CC", "BB",
+  "exit", "print", "putc", "getc",
   "floor", "ceil", "abs", "neg",
   "+", "-", "*", "/", "%", "^"
 };
@@ -34,95 +40,64 @@ static inline bool primitive(word w) {
   return(primin <= w.prim && w.prim < primax);
 }
 
-static void dump(cell c) {
-  if(primitive(c[0])) {
-    if(c[0].prim == prim_number) {
-      printf("%g", c[1].num);
-      return;
-    }
-    printf("%s", primname[c[0].prim]);
-  } else {
-    dump(c[0].ptr);
-  }
-  if(primitive(c[1])) {
-    printf(" %s", primname[c[1].prim]);
-  } else {
-    printf(" (");
-    dump(c[1].ptr);
-    printf(")");
-  }
-}
+typedef enum { right, left } either;
 
-static void rdump(word w) {
+static void dump(word w, either in, either mode) {
+  cell c = w.ptr;
   if(primitive(w)) {
-    printf(" %s", primname[w.prim]);
+    printf("%s", primname[w.prim]);
+  } else if(c[0].prim == special_number) {
+    printf("%g", c[1].num);
   } else {
-    if(primitive(w.ptr[0])) {
-      printf(" %s", primname[w.ptr[0].prim]);
-    } else {
-      printf(" (");
-      dump(w.ptr[0].ptr);
-      printf(")");
-    }
-    rdump(w.ptr[1]);
+    if(in != mode) printf("(");
+    dump(c[0], left, left);
+    printf(" ");
+    dump(c[1], right, mode);
+    if(in != mode) printf(")");
   }
 }
 
 static void edump(cell c) {
-  if(primitive(c[0])) {
-    if(c[0].prim == prim_number) {
-      printf("%g", c[1].num);
-      return;
-    }
-    printf("%s", primname[c[0].prim]);
-  } else {
-    dump(c[0].ptr);
-  }
-  printf(" --");
-  rdump(c[1]);
+  dump(c[0], left, left);
+  printf(" -- ");
+  dump(c[1], right, right);
   printf("\n");
 }
 
 static cell cheney(cell root) {
   word *new = malloc(heap_size * sizeof(*new));
-
   word *ptr = new;
   ptr[0] = root[0];
   ptr[1] = root[1];
   ptr += 2;
-
   /* scan new heap word-by-word */
   for(word *here = new; here < ptr; here++) {
     if(primitive(*here)) {
       /* skip both words of a boxed number */
-      if(here->prim == prim_number)
+      if(here->prim == special_number)
 	here++;
       continue;
     }
     cell there = here->ptr;
-    if(there[0].ptr == NULL) {
+    if(there[0].prim == special_forward) {
       /* already copied */
       *here = there[1];
     } else {
       here->ptr = ptr;
       ptr[0] = there[0];
       ptr[1] = there[1];
-      there[0].ptr = NULL;
+      there[0].prim = special_forward;
       there[1].ptr = ptr;
       ptr += 2;
     }
   }
-
-  free(heap_lo);
-
   printf("cheney copied %d words\n", ptr - new);
-
+  free(heap_lo);
   heap_lo = new;
   heap_hi = new + heap_size;
   heap_ptr = ptr;
-  if(ptr > new + heap_size / 2)
+  while(ptr > new + heap_size / 2)
     heap_size *= 2;
-
   return(new);
 }
 
@@ -143,10 +118,8 @@ static inline word cons(word w0, word w1) {
   return(w);
 }
 
-static inline cell unwind(cell c, word *arg) {
+static inline cell rewind(cell c, word *arg) {
   cell prev = c[1].ptr;
-  if(prev == NULL)
-    return(NULL);
   *arg = c[1] = prev[0];
   prev[0].ptr = c;
   return(prev);
@@ -154,25 +127,24 @@ static inline cell unwind(cell c, word *arg) {
 
 static inline cell numarg(cell c, double *arg) {
   word box;
-  c = unwind(c, &box);
-  if(c == NULL)
-    return(NULL);
-  assert(!primitive(box) && box.ptr[0].prim == prim_number);
+  c = rewind(c, &box);
+  assert(!primitive(box) && box.ptr[0].prim == special_number);
   *arg = box.ptr[1].num;
   return(c);
 }
 
 static inline void numval(cell c, double val) {
   cell n = c[0].ptr;
-  n[0].prim = prim_number;
+  n[0].prim = special_number;
   n[1].num = val;
 }
 
 static void eval(cell c) {
-  while(c) {
+  for(;;) {
     edump(c);
     switch(c[0].prim) {
       default: {
+	/* unwind */
 	cell next = c[0].ptr;
 	c[0] = next[1];
 	next[1].ptr = c;
@@ -180,28 +152,28 @@ static void eval(cell c) {
       } continue;
       case(prim_I): {
 	word arg;
-	c = unwind(c, &arg);
+	c = rewind(c, &arg);
 	if(c) c[0] = arg;
       } continue;
       case(prim_J): {
 	word t, f;
-	c = unwind(c, &t);
-	c = unwind(c, &f);
+	c = rewind(c, &t);
+	c = rewind(c, &f);
 	if(c) c[0] = f;
       } continue;
       case(prim_K): {
 	word t, f;
-	c = unwind(c, &t);
-	c = unwind(c, &f);
+	c = rewind(c, &t);
+	c = rewind(c, &f);
 	if(c) c[0] = t;
       } continue;
       case(prim_S): {
 	word f, g, x;
 	cell r;
 	c = need(c, 2);
-	c = unwind(c, &f);
-	c = unwind(c, &g);
-	c = unwind(r=c, &x);
+	c = rewind(c, &f);
+	c = rewind(c, &g);
+	c = rewind(r=c, &x);
 	if(c) {
 	  r[0] = cons(f,x);
 	  r[1] = cons(g,x);
@@ -210,30 +182,33 @@ static void eval(cell c) {
       case(prim_Y): {
 	word fun;
 	cell r;
-	c = unwind(r=c, &fun);
+	c = rewind(r=c, &fun);
 	if(c) {
 	  r[0] = fun;
 	  r[1].ptr = r;
 	}
       } continue;
-      case(prim_number): {
+      case(special_number): {
 	word n, k;
 	cell r;
-	c = unwind(c, &n); /* unboxed */
-	c = unwind(r=c, &k);
+	c = rewind(c, &n); /* unboxed */
+	c = rewind(r=c, &k);
 	if(c) {
 	  n = r[0]; /* boxed */
 	  r[0] = k;
 	  r[1] = n;
 	}
       } continue;
+      case(prim_exit): {
+	exit(0);
+      } continue;
       case(prim_print): {
 	double v;
 	word k, w;
 	cell r;
 	c = numarg(c, &v);
-	c = unwind(c, &k);
-	c = unwind(r=c, &w);
+	c = rewind(c, &k);
+	c = rewind(r=c, &w);
 	if(c) {
 	  printf("%g\n", v);
 	  /* print n k w -> k w */
@@ -275,9 +250,9 @@ static void eval(cell c) {
 word IO[] = {
   [0].ptr = IO+2, [1].ptr = NULL,
   [2].ptr = IO+4, [3].prim = prim_I, // w
-  [4].ptr = IO+6, [5].prim = prim_I, // k
+  [4].ptr = IO+6, [5].prim = prim_exit, // k
   [6].ptr = IO+10, [7].ptr = IO+8,
-  [8].prim = prim_number, [9].num = 42,
+  [8].prim = special_number, [9].num = 42,
   [10].ptr = IO+12, [11].prim = prim_print,
   [12].ptr = IO+14, [13].prim = prim_K,
   [14].prim = prim_S, [15].prim = prim_K,
@@ -286,5 +261,5 @@ word IO[] = {
 int main(void) {
   heap_size = 2*sizeof(IO)/sizeof(*IO);
   eval(IO);
-  return(0);
+  return(1);
 }
