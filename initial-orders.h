2fd00cf6cefee9b3235c8998dd4856ec6f994f37
Tony Finch <dot@dotat.at>
2011-07-05 23:11:34 +0100

    Another test...

--- initial-orders.h
+++ initial-orders.h
@@ -4,14 +4,11 @@
 word initial_orders[] = {
   IO(1), IOprim(I),
   IO(2), IOprim(exit),
-  IO(3), IOprim(print),
+  IO(7), IO(3),
   IO(5), IO(4),
   IOprim(box_num), IOnum(2),
-  IO(7), IO(6),
+  IOprim(add), IO(6),
   IOprim(box_num), IOnum(1),
-  IO(8), IOprim(add),
-  IO(10), IO(9),
-  IOprim(C), IOprim(B),
-  IOprim(B), IO(11),
-  IOprim(C), IOprim(B),
+  IO(8), IOprim(print),
+  IOprim(C), IOprim(I),
 };
