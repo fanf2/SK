36ee0afaa156cceae5022bc44293e9168fd5e612
Tony Finch <dot@dotat.at>
2011-07-05 23:05:18 +0100

    More interesting test

--- initial-orders.h
+++ initial-orders.h
@@ -1,11 +1,17 @@
-#define IO initial_orders
+#define IO(n)     { .ptr = initial_orders+n*2 }
+#define IOprim(n) { .prim = prim_##n }
+#define IOnum(n)  { .num  = n }
 word initial_orders[] = {
-  [0].ptr = IO+2, [1].ptr = NULL,
-  [2].ptr = IO+4, [3].prim = prim_I, // w
-  [4].ptr = IO+6, [5].prim = prim_exit, // k
-  [6].ptr = IO+10, [7].ptr = IO+8,
-  [8].prim = prim_box_num, [9].num = 42,
-  [10].ptr = IO+12, [11].prim = prim_print,
-  [12].ptr = IO+14, [13].prim = prim_K,
-  [14].prim = prim_S, [15].prim = prim_K,
+  IO(1), IOprim(I),
+  IO(2), IOprim(exit),
+  IO(3), IOprim(print),
+  IO(5), IO(4),
+  IOprim(box_num), IOnum(2),
+  IO(7), IO(6),
+  IOprim(box_num), IOnum(1),
+  IO(8), IOprim(add),
+  IO(10), IO(9),
+  IOprim(C), IOprim(B),
+  IOprim(B), IO(11),
+  IOprim(C), IOprim(B),
 };
