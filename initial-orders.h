68cb9a2aa0e043d06deffeeab805c4de7d3880ec
Tony Finch <dot@dotat.at>
2011-07-06 14:09:31 +0100

    An implementation of cat

--- initial-orders.h
+++ initial-orders.h
@@ -3,19 +3,33 @@
 #define IOnum(n)  { .num  = n }
 word initial_orders[] = {
   IO(1), IOprim(I),
-  IO(2), IOprim(exit),
-  IO(3), IOprim(print),
-  IO(5), IO(4),
-  IO(14), IO(12),
-  IO(7), IO(6),
-  IO(14), IO(13),
-  IO(8), IOprim(add),
-  IO(10), IO(9),
+  IO(2), IO(8),
+  IO(3), IO(4),
+  IOprim(S), IO(4),
+  IO(5), IO(6),
   IOprim(C), IOprim(B),
-  IOprim(B), IO(11),
-  IOprim(C), IOprim(B),
-  IOprim(box_num), IOnum(3),
-  IOprim(box_num), IOnum(2),
-  IOprim(add), IO(15),
-  IOprim(box_num), IOnum(1),
+  IO(7), IOprim(I),
+  IOprim(S), IOprim(I),
+  IO(9), IO(16),
+  IO(10), IO(11),
+  IOprim(BB), IOprim(getc),
+  IOprim(S), IO(12),
+  IO(13), IOprim(exit),
+  IOprim(C), IO(14),
+  IOprim(gt), IO(15),
+  IOprim(box_num), IOnum(0),
+  IOprim(C), IOprim(putc),
 };
+
+// `X( `fun(X `arg(proc proc arg))
+//     `fun(X `arg(proc proc arg)) )
+//
+// Y = S (C B (S I I)) (C B (S I I));
+//
+// main = Y \loop (
+//  getc \c
+//  > 0 c;
+//    exit;
+//    putc c loop);
+//
+// main = Y (BB getc (S (C (gt 0) exit)) (C putc))
