342142f8e54d41d638837d3d43cf03c7a5023777
Tony Finch <dot@dotat.at>
2011-07-07 01:54:41 +0100

    Prototype parser for fexl-ish lambda expressions

---
+++ parse.hs
@@ -0,0 +1,46 @@
+import Text.ParserCombinators.Parsec
+
+infixl 9 :@:
+
+data Expr = Expr :@: Expr | Lambda String Expr | Var String | Num Integer
+     deriving (Eq, Read, Show)
+
+tryJoin f p qq = do pp <- p
+                    return (f qq pp)
+                 <|> return qq
+
+expr = lambda <|> (terms >>= tryJoin (:@:) (lambda <|> rest))
+
+rest = ch ';' >> expr
+
+lambda = do ch '\\' <|> ch '`'
+            var <- name
+            lambda_let var <|> lambda_fun var
+
+lambda_let var = do ch '='
+                    arg <- terms
+                    val <- rest
+                    return (Lambda var val :@: arg)
+
+lambda_fun var = do val <- expr
+                    return (Lambda var val)
+
+terms = chainl1 term $ return (:@:)
+
+term = brac <|> num <|> var <?> "term"
+
+brac = between (ch '(') (ch ')') expr
+
+num = do digits <- many1 digit
+         spret $ Num (read digits)
+
+var = do n <- name
+         return $ Var n
+
+name = do a <- letter
+          as <- many alphaNum
+          spret (a:as)
+
+spret v = spaces >> return v
+
+ch c = char c >> spaces
